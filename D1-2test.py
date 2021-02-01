4f5cf425dc68e9115a72a50ceb2b4d4336c279a9
kevinchou1 <78331134+kevinchou1@users.noreply.github.com>
2021-02-01 10:43:43 +0800

    Add files via upload

---
+++ D1-2test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:02:32 2021
+
+@author: MTAEXAM
+"""
+
+height=float(input("高:"))
+top=float(input("上底:"))
+botton=float(input("下底:"))
+area=(top+botton)*height/2
+print("梯形面積:"+str(area))
